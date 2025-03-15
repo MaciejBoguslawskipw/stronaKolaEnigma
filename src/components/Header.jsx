c9e239326fb1b2f47d75103e1b05a97ab59c371b
Maciej Bogusławski <01200725@pw.edu.pl>
2025-03-15 20:19:11 +0100

    Add burger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/global.css';
+import '../styles/burger.css'; // Importujemy nowy plik CSS
 
 const Header = () => {
-    return (
-      <header>
-        <h1>Koło Naukowe Enigma</h1>
-        <nav>
-          <a href="/">Strona główna</a>
-          <a href="/projects">Projekty</a>
-          <a href="/collaborations">Współprace</a>
-          <a href="/memes">Memy z IT</a>
-          <a href="/contact">Kontakt</a>
-          <a href="/management">Zarząd</a>
-          <a href="/downloadmoreram">Pobierz RAM</a>
-        </nav>
-      </header>
-    );
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
-  
-  export default Header;
+
+  return (
+    <header>
+      <div className="header-content">
+        <h1>Koło Naukowe Enigma</h1>
+        <button className="hamburger" onClick={toggleMenu}>
+          <div className={`hamburger-line ${isMenuOpen ? 'open' : ''}`}></div>
+          <div className={`hamburger-line ${isMenuOpen ? 'open' : ''}`}></div>
+          <div className={`hamburger-line ${isMenuOpen ? 'open' : ''}`}></div>
+        </button>
+      </div>
+      <nav className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+        <a href="/">Strona główna</a>
+        <a href="/projects">Projekty</a>
+        <a href="/collaborations">Współprace</a>
+        <a href="/memes">Memy z IT</a>
+        <a href="/contact">Kontakt</a>
+        <a href="/management">Zarząd</a>
+        <a href="/downloadmoreram">Pobierz RAM</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
