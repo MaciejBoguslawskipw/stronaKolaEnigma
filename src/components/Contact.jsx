596935bd80060996b00771625ce75d844d618dcf
Maciej Bogusławski <01200725@pw.edu.pl>
2025-03-15 20:57:16 +0100

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ const Contact = () => {
     return (
       <div>
         <h1>Kontakt</h1>
-        <p>Email: enigma@example.com</p>
+        <p>Email: kn.enigma@pw.edu.pl</p>
         <p>Telefon: +48 123 456 789</p>
       </div>
     );
